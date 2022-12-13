10063e2db0ef4d015f07d7d16aef437093b29fe9
Marcus Castilho <61066188+castpy@users.noreply.github.com>
2022-12-13 14:46:57 -0300

    Mario Game v1.0

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+  mario.classList.add('jump');
+
+  setTimeout(() => {
+    mario.classList.remove('jump');
+  }, 500);
+}
+
+
+const loop = setInterval(() => {
+    const pipe_position = pipe.offsetLeft;
+    const mario_position = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+
+    if (pipe_position <= 123 && pipe_position > 0 && mario_position <= 60){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipe_position}px`
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${pipe_position}px`
+
+        mario.src = "/IMG/mario-game-over.png"
+        mario.style.width = '100px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop)
+    }
+
+}, 10)
+
+
+document.addEventListener('keydown', jump);
